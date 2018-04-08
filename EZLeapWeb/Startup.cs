64ca4c9c3d155836f124911e858dd5af7a393fd2
Elena V. Evdokimenko <el.evdokimenko@gmail.com>
2018-04-08 22:40:22 +0100

    Add project files.

---
+++ EZLeapWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EZLeapWeb.Startup))]
+namespace EZLeapWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
